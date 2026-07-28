35ed6dcab971e1d71e0f8e39b72a1d0e1fd7a5f5
中ジョ <nakamura.johnielo@gmail.com>
2026-07-28 18:42:24 -0500

    ui:headercell modal

--- src/components/table/HeaderCell.tsx
+++ src/components/table/HeaderCell.tsx
@@ -1,24 +1,32 @@
-import type { ComponentProps, ComponentPropsWithRef, ReactNode } from "react";
+import {
+  useRef,
+  type ComponentProps,
+  type ComponentPropsWithRef,
+  type ReactNode,
+} from "react";
 import { cn } from "@/shared/lib/util";
 import { IconName, Icons } from "../icon";
 import IconButton from "../button/IconButton";
-import { DataViewColumn, DataViewConfig } from "./dataView.types";
+import { DataViewColumn } from "./dataView.types";
 import { Column } from "@tanstack/react-table";
 import ButtonModal from "../ButtonModal";
 import { TableColumnFilterContainer } from "./DataTable/TableColumnFilterContainer";
+import { getTableSortIcon } from "./DataTable/useDataTable";
+import Modal, { ModalProvider, type ModalHandle } from "../modal/Modal";
 
 type ColumnActionsProps<TItem> = {
   column: Column<TItem>;
   config: DataViewColumn<TItem>;
   compact: boolean;
-  menuOpen: boolean;
-  filterOpen: boolean;
-  onFilterClose: () => void;
-  onFilterToggle: (columnId: string) => void;
   onHide: (column: Column<TItem>) => void;
-  onMenuOpenChange: (columnId: string, open: boolean) => void;
   onPin: (column: Column<TItem>) => void;
   onSort: (column: Column<TItem>) => void;
+  //
+  menuOpen?: boolean;
+  filterOpen?: boolean;
+  onFilterClose?: () => void;
+  onFilterToggle?: (columnId: string) => void;
+  onMenuOpenChange?: (columnId: string, open: boolean) => void;
 };
 
 type HeaderCellProps<TItem> = ComponentProps<"div"> &
@@ -44,17 +52,19 @@ export default function HeaderCell<TItem>({
   column,
   config,
   compact,
-  menuOpen,
-  filterOpen,
-  onFilterClose,
-  onFilterToggle,
   onHide,
-  onMenuOpenChange,
   onPin,
   onSort,
 
   ...props
 }: HeaderCellProps<TItem>) {
+  compact = true;
+
+  const cellRef = useRef<HTMLDivElement>(null);
+  const menuButtonRef = useRef<HTMLButtonElement>(null);
+  const menuModalRef = useRef<ModalHandle>(null);
+  const filterModalRef = useRef<ModalHandle>(null);
+  const menuButtonId = "menu-button";
   const content = children ?? label;
   const contentText =
     typeof content === "string" || typeof content === "number"
@@ -65,25 +75,35 @@ export default function HeaderCell<TItem>({
   const IconComponent = icon && Icons[icon];
 
   const menuItems = [
-    { icon: "pin", label: "pivot", onClick: () => onPin(column) },
-    { icon: "unvisible", label: "ocultar", onClick: () => onHide(column) },
+    {
+      icon: "pin",
+      label: "pivot",
+      onClick: () => onPin(column),
+    },
+    {
+      icon: "unvisible",
+      label: "ocultar",
+      onClick: () => onHide(column),
+    },
     {
       icon: "filter",
       label: "filtro",
-      onClick: () => onFilterToggle(column.id),
+      onClick: () =>
+        filterModalRef.current?.open(menuButtonRef.current ?? undefined),
     },
   ] satisfies ItemProps[];
+
   return (
     <div
+      ref={cellRef}
       className={cn(
         "flex h-9 items-center border border-DividerMiddle bg-DividerLowest px-2.5 text-xs font-medium text-OnSurface/60",
-        // leading ? "justify-between gap-2" : "justify-start",
         className,
       )}
+      onClick={() => menuModalRef.current?.open(cellRef.current ?? undefined)}
       {...props}
     >
       <div className="flex gap-[3px] items-center">
-        {/* {leading ? <div className="shrink-0 size-4.5">{leading}</div> : null} */}
         {IconComponent && <IconComponent className="size-4.5" />}
         <div
           className="min-w-0 flex-1 truncate text-sm"
@@ -93,112 +113,97 @@ export default function HeaderCell<TItem>({
         </div>
       </div>
 
-      {actions ??
-        (showDefaultActions && (
-          <div className="flex items-center gap-3 ml-auto shrink-0">
-            {/* if has spacing, visible */}
-            {compact && (
-              <div className="flex gap-0.5 shrink-0 h-fit">
-                <IconButton
-                  icon="pin"
-                  size={"md"}
-                  appearance={"text"}
-                  intent="lightInk"
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    onPin(column);
-                  }}
-                />
-                <IconButton
-                  icon="unvisible"
-                  size={"md"}
-                  appearance={"text"}
-                  intent="lightInk"
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    onHide(column);
-                  }}
-                />
-                {config.filterable !== false && (
-                  <ButtonModal
-                    open={filterOpen}
-                    onOpenChange={() => {
-                      onFilterToggle(column.id);
-                    }}
-                  >
-                    <ButtonModal.Trigger>
-                      <IconButton
-                        icon="filter"
-                        size={"md"}
-                        appearance={"text"}
-                        intent="lightInk"
-                        onClick={(event) => {
-                          event.stopPropagation();
-                        }}
-                      />
-                    </ButtonModal.Trigger>
-                    <ButtonModal.Content>
-                      {filterOpen ? (
-                        <div className="absolute right-0 top-9 z-40">
-                          <TableColumnFilterContainer
-                            column={column}
-                            config={config}
-                          />
-                        </div>
-                      ) : (
-                        <div />
-                      )}
-                    </ButtonModal.Content>
-                  </ButtonModal>
-                )}
-              </div>
-            )}
-
-            {/* always visible */}
-            <div className="shrink-0  flex gap-1">
+      <ModalProvider ref={filterModalRef}>
+        <Modal.Content>
+          <TableColumnFilterContainer column={column} config={config} />
+        </Modal.Content>
+        {actions ??
+          (showDefaultActions && (
+            <div className="flex items-center gap-3 ml-auto shrink-0">
+              {/* if has spacing, visible */}
               {!compact && (
-                // <IconButtonOLD>
-                //   <ThreePointMenuIcon className="size-search-filter-dismiss" />
-                // </IconButtonOLD>
-
-                <ButtonModal
-                  open={menuOpen}
-                  onOpenChange={(open) => onMenuOpenChange(column.id, open)}
-                >
-                  <ButtonModal.Trigger>
-                    <IconButton
-                      icon="threePointMenu"
-                      size={"md"}
-                      appearance={"text"}
-                      intent="lightInk"
-                    />
-                  </ButtonModal.Trigger>
-                  <ButtonModal.Content>
-                    <ColumnToolMenu items={menuItems} />
-                  </ButtonModal.Content>
-                </ButtonModal>
+                <div className="flex gap-0.5 shrink-0 h-fit">
+                  <IconButton
+                    icon="pin"
+                    size={"md"}
+                    appearance={"text"}
+                    intent="lightInk"
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      onPin(column);
+                    }}
+                  />
+                  <IconButton
+                    icon="unvisible"
+                    size={"md"}
+                    appearance={"text"}
+                    intent="lightInk"
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      onHide(column);
+                    }}
+                  />
+                  {config.filterable !== false && (
+                    <>
+                      <Modal.Trigger>
+                        <IconButton
+                          icon="filter"
+                          size={"md"}
+                          appearance={"text"}
+                          intent="lightInk"
+                          onClick={(event) => {
+                            event.stopPropagation();
+                          }}
+                        />
+                      </Modal.Trigger>
+                    </>
+                  )}
+                </div>
               )}
-              {/* <IconButtonOLD>
-            <TextAscendingIcon className="size-5" />
-          </IconButtonOLD> */}
-              <IconButton
-                icon="textAscending"
-                size={"md"}
-                appearance={"text"}
-                intent="lightInk"
-                onClick={() => onSort(column)}
-              />
-            </div>
-          </div>
-        ))}
 
-      {/* <button
-        aria-label={`${columnConfig.label}の幅を変更`}
-        className="h-full w-1 cursor-col-resize self-stretch bg-transparent hover:bg-Primary"
-        onClick={(event) => event.stopPropagation()}
-        onMouseDown={(event) => onResize(header, event)}
-        type="button"
-      /> */}
+              {/* always visible */}
+              <div className="shrink-0  flex gap-1">
+                {compact && (
+                  <>
+                    {/* <Modal.Reference id={menuButtonId} /> */}
+                    <ButtonModal ref={menuModalRef}>
+                      <ButtonModal.Trigger>
+                        <IconButton
+                          ref={menuButtonRef}
+                          icon="threePointMenu"
+                          size={"md"}
+                          appearance={"text"}
+                          intent="lightInk"
+                          onClick={(event) => {
+                            event.stopPropagation();
+                          }}
+                        />
+                      </ButtonModal.Trigger>
+
+                      <ButtonModal.Content>
+                        <ColumnToolMenu
+                          items={menuItems}
+                          onItemSelect={() => menuModalRef.current?.close()}
+                        />
+                      </ButtonModal.Content>
+                    </ButtonModal>
+                  </>
+                )}
+
+                <IconButton
+                  icon={getTableSortIcon(column, config)}
+                  size={"md"}
+                  appearance={"text"}
+                  intent="lightInk"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    onSort(column);
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+      </ModalProvider>
     </div>
   );
 }
@@ -206,12 +211,45 @@ export default function HeaderCell<TItem>({
 type ItemProps = {
   icon: IconName;
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 };
 type ColumnToolMenuProps = ComponentPropsWithRef<"div"> & {
   items: ItemProps[];
+  onItemSelect?: () => void;
 };
-function ColumnToolMenu({ items, ...props }: ColumnToolMenuProps) {
+type ButtonItemProps = ComponentProps<"button"> & {
+  item: ItemProps;
+  onItemSelect?: () => void;
+};
+
+function ColumnToolMenu({
+  items,
+  onItemSelect,
+  ...props
+}: ColumnToolMenuProps) {
+  function ButtonItem({
+    item,
+    onItemSelect,
+    onClick,
+    ...props
+  }: ButtonItemProps) {
+    const Icon = Icons[item.icon];
+    return (
+      <button
+        className="flex items-center gap-2 rounded-sm px-2 py-1.5 text-left text-sm hover:bg-Primary hover:text-OnPrimary"
+        type="button"
+        {...props}
+        onClick={(event) => {
+          onClick?.(event);
+          item.onClick?.();
+          onItemSelect?.();
+        }}
+      >
+        <Icon className="size-4" /> {item.label}
+      </button>
+    );
+  }
+
   // absolute left-0 top-9 z-40
   return (
     <div
@@ -219,16 +257,12 @@ function ColumnToolMenu({ items, ...props }: ColumnToolMenuProps) {
       {...props}
     >
       {items.map((item, index) => {
-        const Icon = Icons[item.icon];
         return (
-          <button
-            className="flex items-center gap-2 rounded-sm px-2 py-1.5 text-left text-sm hover:bg-Primary hover:text-OnPrimary"
+          <ButtonItem
             key={item.label + index}
-            onClick={item.onClick}
-            type="button"
-          >
-            <Icon className="size-4" /> {item.label}
-          </button>
+            item={item}
+            onItemSelect={onItemSelect}
+          />
         );
       })}
     </div>
