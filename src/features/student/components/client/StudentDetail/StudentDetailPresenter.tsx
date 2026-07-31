c1b7906644bb4780191185122e3feb346e856353
中ジョ <nakamura.johnielo@gmail.com>
2026-07-31 01:37:44 -0500

    ui: diagram displayed

--- src/features/student/components/client/StudentDetail/StudentDetailPresenter.tsx
+++ src/features/student/components/client/StudentDetail/StudentDetailPresenter.tsx
@@ -1,6 +1,4 @@
-import type {
-  StudentClassItem,
-} from "@/features/student/types";
+import type { StudentClassItem } from "@/features/student/types";
 import type { StudentDetail } from "../../ui/studentSummaryPanel/studentSummary.type";
 import ContentTitleSection from "@/components/ContentTitleSection";
 import DataSection from "@/components/datasection/DataSection";
@@ -39,14 +37,12 @@ export function StudentDetailPresenter({
     return <div>Loading...</div>;
   }
 
-  if ( !studentDetail) {
+  if (!studentDetail) {
     return <div>Student not found</div>;
   }
 
-
-
   return (
-   <div className="flex flex-col gap-3 pb-0 w-full h-full">
+    <div className="flex flex-col gap-3 pb-0 w-full h-full">
       <ContentTitleSection title={"Alumnos"} />
       <DataSection
         className="w-full flex-1 min-h-0"
@@ -57,8 +53,8 @@ export function StudentDetailPresenter({
         tableConfig={tableConfig}
         listDiagram={<DataTable config={tableConfig} table={table} />}
         cardDiagram={
-          // <StudentDiagram loading={loading} items={studentGrades} />
-          <div>DIAGRAM</div>
+          <StudentDiagram loading={loading} items={studentGrades} />
+          // <div>DIAGRAM</div>
         }
       />
     </div>
